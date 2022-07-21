6c8b669304eb5cfbdf8f2b1b64fabf4a65f2460f
jonathan A <jonathanarias0321@gmail.com>
2022-07-21 19:29:16 -0400

    This my answer

---
+++ BMI.py
@@ -0,0 +1,6 @@
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+new_height = float(height)
+new_weight = float(weight)
+BMI = (new_weight) / (new_height ** 2)
+print(int(BMI))
